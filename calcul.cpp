b9d5fafc86a3d2c616110956a3181228b0dd7876
Антон Федотов <94544940+Amikan93@users.noreply.github.com>
2021-12-08 19:19:59 +0600

    Create calcul.cpp

---
+++ calcul.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+void mult(int x, int y, int result) {
+    result = x * y;
+    std::cout << result << std::endl;
+}
+
+void divi(int x, int y, int result) {
+    result = x / y;
+    std::cout << result << std::endl;
+}
+
+void add(int x, int y, int result) {
+    result = x + y;
+    std::cout << result << std::endl;
+}
+
+void sub(int x, int y, int result) {
+    result = x - y;
+    std::cout << result << std::endl;
+}
+
+int main() {
+    int x;
+    int y;
+    int result;
+
+}
